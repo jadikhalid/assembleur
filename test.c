690a1ae21cc83c46a8c1558867463292927b19a6
Jadi Khalid <jadikhalid@gmail.com>
2026-02-23 11:23:15 +0000

    new changes

--- test.c
+++ test.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
 
-int count_bits(unsigned int data)
-{
-    int cnt = 0;
+static unsigned char byte_bit_count[256]; /* tableau de recherche */
 
-    while (data != 0)
+void initialize_count_bits()
+{
+    int cnt, i, data;
+
+    for (i = 0; i < 256; i++)
     {
-        data = data & (data - 1);
-        cnt++;
+        cnt = 0;
+        data = i;
+        while (data != 0)
+        { /* methode une */
+            data = data & (data - 1);
+            cnt++;
+        }
+        byte_bit_count[i] = cnt;
     }
-    return cnt;
 }
 
+int count_bits(unsigned int data)
+{
+    const unsigned char *byte = (unsigned char *)&data;
+
+    return byte_bit_count[byte[0]] + byte_bit_count[byte[1]] +
+           byte_bit_count[byte[2]] + byte_bit_count[byte[3]];
+}
 int main(void)
 {
+    initialize_count_bits();
     unsigned int data = 22;
     int result = count_bits(data);
     printf("Nombre de bit : %d\n", result);

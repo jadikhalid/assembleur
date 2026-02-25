686c6e475d8486871459d8394aa7a070ab979154
jadikhalid <jadikhalid@gmail.com>
2026-02-25 12:00:00 +0000

    changements

--- driver.c
+++ driver.c
@@ -1,7 +1,6 @@
 int asm_main(void);
 
 int main() {
-    int ret_status;
-    ret_status = asm_main();
-    return ret_status;
+    asm_main();
+    return 0;
 }
--- sub1.asm
+++ sub1.asm
@@ -3,5 +3,65 @@
 %include "asm_io.inc"
 
 segment .data
-prompt1 db "Enter an integer: ", 0
+prompt1 db "Entrez un nombre: ", 0
+prompt2 db "Entrez un autre nombre: ", 0
+outmsg1 db "Vous avez entré ",0
+outmsg2 db " et ",0
+outmsg3 db ", la somme des deux vaut : ",0
 
+segment .bss
+input1 resd 1
+input2 resd 1
+
+segment .text
+    global _asm_main
+_asm_main:
+    enter   0,0
+    pusha
+    mov eax, prompt1
+    call print_string
+
+    mov ebx, input1
+    mov ecx, ret1
+    jmp short get_int
+
+ret1:
+    mov eax, prompt2
+    call print_string
+
+    mov ebx, input2
+    mov ecx, $ + 7
+    jmp short get_int
+
+    mov eax, [input1]
+    add eax, [input2]
+    mov ebx, eax
+    mov eax, outmsg1
+    call print_string
+    mov eax, [input1]
+    call print_int
+    mov eax, outmsg2
+    call print_string
+    mov eax, [input2]
+    call print_int
+    mov eax, outmsg3
+    call print_string
+    mov eax, ebx
+    call print_int
+    call print_nl
+
+    popa
+    mov eax, 0
+    leave
+    ret
+
+; Subprogram get_int
+; Parameters:
+;   ebx: address of the variable to store the input
+;   ecx: return address
+; Notes :
+; la valeur de eax est écrasée par la fonction read_int, il faut la sauvegarder avant l'appel
+get_int:
+    call read_int
+    mov [ebx], eax
+    jmp ecx
